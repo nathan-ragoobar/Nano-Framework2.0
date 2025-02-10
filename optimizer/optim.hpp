2a9bba19be0cf7e520f3c85d0a9c642a5878d495
Nathan Ragoobar <nathan.ragoobar@my.uwi.edu>
2025-02-10 01:14:22 -0400

    I think it's working with the two datatypes now but I'm getting a division by zero error now

--- optimizer/optim.hpp
+++ optimizer/optim.hpp
@@ -166,36 +166,63 @@ struct AdamW {
         parameter->ZeroGrad();
       }
     }
+
   
-
     void Step(int t) {
-        for (size_t i = 0; i < parameters_.size(); ++i) {
-            auto parameter = parameters_[i]->flat<Type>();
-            auto grad = parameters_[i]->flat_grad<Type>();
-            auto m = m_[i]->flat<Type>();
-            auto v = v_[i]->flat<Type>();
+      for (size_t i = 0; i < parameters_.size(); ++i) {
+          auto dtype = parameters_[i]->dtype();
+          
+          if (dtype == nn::DT_FIXED) {
+              StepParameter<fpm::fixed_16_16>(i, t);
+          } else {
+              StepParameter<float>(i, t);
+          }
+      }
+  }
 
-            // Convert constants to parameter type
-            Type beta1(beta1_);
-            Type beta2(beta2_);
-            Type eps(eps_);
-            Type lr(lr_);
-            Type weight_decay(weight_decay_);
-
-            // Update moments using parameter type
-            m.device(nn::g_device) = beta1 * m + (Type(1.0f) - beta1) * grad;
-            v.device(nn::g_device) = beta2 * v + (Type(1.0f) - beta2) * grad * grad;
-            
-            auto m_hat = m / Type(1.0f - std::pow(beta1_, t));
-            auto v_hat = v / Type(1.0f - std::pow(beta2_, t));
-
-            parameter.device(nn::g_device) -=
-                lr * (m_hat / (v_hat.sqrt() + eps) + weight_decay * parameter);
-        }
-    }
-    
+        
 
 private:
+
+    template<typename T>
+    void StepParameter(size_t i, int t) {
+        auto parameter = parameters_[i]->flat<T>();
+        auto grad = parameters_[i]->flat_grad<T>();
+        auto m = m_[i]->flat<T>();
+        auto v = v_[i]->flat<T>();
+
+        // Convert constants to parameter type
+        T beta1(beta1_);
+        T beta2(beta2_);
+        T eps(eps_);
+        T lr(lr_);
+        T weight_decay(weight_decay_);
+
+        // Bias correction terms
+        T bc1(1.0f - std::pow(beta1_, t));
+        T bc2(1.0f - std::pow(beta2_, t));
+        
+        // Update rule remains the same but uses templated type T
+        for (int j = 0; j < parameter.size(); j++) {
+            // Update biased first moment estimate
+            m.data()[j] = beta1 * m.data()[j] + (T(1.0f) - beta1) * grad.data()[j];
+            
+            // Update biased second raw moment estimate
+            v.data()[j] = beta2 * v.data()[j] + (T(1.0f) - beta2) * grad.data()[j] * grad.data()[j];
+            
+            // Compute bias-corrected first moment estimate
+            T m_hat = m.data()[j] / bc1;
+            
+            // Compute bias-corrected second raw moment estimate
+            T v_hat = v.data()[j] / bc2;
+            
+            // Update parameters
+            parameter.data()[j] = parameter.data()[j] - 
+                                lr * (m_hat / (T(std::sqrt(float(v_hat))) + eps) + 
+                                weight_decay * parameter.data()[j]);
+        }
+    }
+
   std::vector<nn::Parameter*> parameters_;
   std::vector<std::unique_ptr<nn::Parameter>> m_;
   std::vector<std::unique_ptr<nn::Parameter>> v_;
